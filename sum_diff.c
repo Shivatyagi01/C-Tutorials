54eef14922528dbf70309f297838af825441786c
shivatyagi01 <shivaworx1@gmail.com>
2025-07-15 21:18:46 +0530

    arithmetic operations

--- sum_diff.c
+++ sum_diff.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-int a, b, sum, diff;
+float a, b, sum, diff, multiply, divide;
 
 int main()
 {
     printf("Enter two digits\n");
-    scanf("%d %d", &a, &b);
+    scanf("%f %f", &a, &b);
 
     sum = a + b;
     diff = b - a;
+    multiply = a * b;
+    divide = a / b;
 
-    printf("The sum of two digits is %d \n", sum);
-    printf("The difference of two digits is %d ", diff);
+    printf("The sum of two digits is %.2f \n", sum);
+    printf("The difference of two digits is %.2f \n", diff);
+    printf("The product of two digits is %.2f \n", multiply);
+    printf("The remainder of two digits is %.2f ", divide);
     return 0;
 }
